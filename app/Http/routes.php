857641d2799fb9cf95bd50e8483c6be6925fbcb4
MUSEPOL\mamani.miguel <mico_0oo0@hotmail.com>
2017-01-24 17:15:33 -0400

    se realizo la instalacion de composer y ahora se sube

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,12 @@
 |
 */
 
+use App\Note;
+
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('notes',function(){
+  $notes=Note::all();
+  return view('note', compact('notes'));
 });
---
+++ resources/views/note.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel</title>
+</head>
+<body>
+    <h2>Notes</h2>
+    <ul>
+        @foreach ($notes as $note)
+        <li>
+            {{ $note->note }}
+        </li>
+        @endforeach
+    </ul>
+    <!--<form method="POST">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}"></input>
+    <textarea></textarea>
+    <button type="submit">Create note</button>
+</form>-->
+</body>
+</html>
