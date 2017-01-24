80516ca948d3245d78f2d110d186dc23db5cd74e
MUSEPOL\mamani.miguel <mico_0oo0@hotmail.com>
2017-01-24 16:12:03 -0400

    Primera y segunda leccion

---
+++ tests/NoteTest.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+use App\Note;
+
+class NoteTest extends TestCase
+{
+
+  public fuction test_notes_list()
+  {
+    //1.- having "las base de las pruebas, las condiciones"
+    Note::create(['note'=>'My first note']);
+    Note::create(['note'=>'My Second note']);
+
+    // 2.- when "definimos las acciones del usuarios"
+    this->visit('notes')
+    //3.- then "donde agregamos las comprobaciones"
+        ->see('My first note')
+        ->see('My second note');
+  }
+}
